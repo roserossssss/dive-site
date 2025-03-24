19a7c1382a73d8fa4241938868963720df759048
roserossssss <156775235+roserossssss@users.noreply.github.com>
2025-03-24 10:47:40 +0800

    UI Update
    - changed font colors
    - adjusted content alignments

--- diver-application/app/dashboard/CertificatePage/page.tsx
+++ diver-application/app/dashboard/CertificatePage/page.tsx
@@ -71,16 +71,16 @@ const DiveCertification = () => {
   return (
     <div className="flex-1 p-5 pt-2 relative">
       {/* Header Section */}
-      <h2 className="text-3xl font-bold text-black">My Dive Certification</h2>
+      <h2 className="text-3xl font-bold text-[#001526]">My Dive Certification</h2>
       <div className="flex items-center justify-between mt-4">
         <div className="flex items-center space-x-2">
-          <img src="/leftarrow.svg" alt="Left Arrow" className="w-6 h-6 cursor-pointer mt-4" />
-          <img src="/rightarrow.svg" alt="Right Arrow" className="w-6 h-6 cursor-pointer mt-4" />
+          <img src="/leftarrow.svg" alt="Left Arrow" className="w-7 h-7 cursor-pointer mt-3" />
+          <img src="/rightarrow.svg" alt="Right Arrow" className="w-7 h-7 cursor-pointer mt-3" />
           <button
             onClick={() => router.push("/dashboard/CertificatePage/NewCertificate")}
-            className="bg-[#001526] text-white px-6 py-2 rounded-full flex items-center gap-2 mt-4"
+            className="bg-[#001526] text-white px-6 py-3 rounded-full flex items-center gap-2 mt-4"
           >
-            <img src="/plus.svg" alt="Plus" className="w-3 h-3 -mt-1" />
+            <img src="/plus.svg" alt="Plus" className="w-3 h-3 -mt-1 -ml-2" />
             New Certificate
           </button>
         </div>
@@ -108,13 +108,13 @@ const DiveCertification = () => {
               <div className="absolute right-20 mt-2 bg-white shadow-lg rounded-lg w-[100px] h-[75px] z-50">
                 <button
                   onClick={() => handleSort("name")}
-                  className="block w-28 text-left px-7 py-3 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-28 text-left px-7 py-3 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-40 mx-auto"
                 >
                   Name
                 </button>
                 <button
                   onClick={() => handleSort("level")}
-                  className="block w-28 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-28 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-40 mx-auto"
                 >
                   Level
                 </button>
@@ -135,79 +135,79 @@ const DiveCertification = () => {
               <div className="absolute right-0 mt-2 bg-white shadow-lg rounded-lg w-[90px] h-[398px] z-50">
                 <button
                   onClick={() => handleFilter(null)}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526]hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   All
                 </button>
                 <button
                   onClick={() => handleFilter("ACUC")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   ACUC
                 </button>
                 <button
                   onClick={() => handleFilter("BSAC")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   BSAC
                 </button>
                 <button
                   onClick={() => handleFilter("CMAS")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   CMAS
                 </button>
                 <button
                   onClick={() => handleFilter("GUE")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   GUE
                 </button>
                 <button
                   onClick={() => handleFilter("IANTD")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   IANTD
                 </button>
                 <button
                   onClick={() => handleFilter("NASE")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   NASE
                 </button>
                 <button
                   onClick={() => handleFilter("NAUI")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   NAUI
                 </button>
                 <button
                   onClick={() => handleFilter("PADI")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   PADI
                 </button>
                 <button
                   onClick={() => handleFilter("RADI")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   RADI
                 </button>
                 <button
                   onClick={() => handleFilter("SDI")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   SDI
                 </button>
                 <button
                   onClick={() => handleFilter("SSI")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   SSI
                 </button>
                 <button
                   onClick={() => handleFilter("TDI")}
-                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-black hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
+                  className="block w-24 text-left px-7 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#001526] hover:bg-opacity-50 mx-auto"
                 >
                   TDI
                 </button>
@@ -222,12 +222,12 @@ const DiveCertification = () => {
         {certificates.length === 0 ? (
           <div className="flex items-center justify-center min-h-[700px] text-center">
             <div>
-              <div className="w-[250px] h-[250px] border-2 border-black rounded-full mx-auto"></div>
-              <p className="text-5xl text-black font-bold mt-20">No Certifications Yet</p>
-              <p className="text-black text-xl mt-9">
+              <div className="w-[250px] h-[250px] border-2 border-[#001526] rounded-full mx-auto"></div>
+              <p className="text-5xl text-[#001526] font-bold mt-20">No Certifications Yet</p>
+              <p className="text-[#001526] text-xl mt-6">
                 You haven’t added any certifications. 
               </p>
-              <p className="text-black text-xl mt-1"> 
+              <p className="text-[#001526] text-xl mt-1"> 
               Start tracking your dive qualifications here!
               </p>
             </div>
@@ -253,7 +253,7 @@ const DiveCertification = () => {
                     <div className="absolute bottom-[-3rem] right-2 bg-white shadow-lg rounded-lg p-2 w-32">
                       <button
                         onClick={() => updateCertificate(cert.id)}
-                        className="block w-full text-left px-4 py-2 rounded-lg text-medium text-black hover:bg-[#2C7DA0] hover:bg-opacity-50"
+                        className="block w-full text-left px-4 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#2C7DA0] hover:bg-opacity-50"
                       >
                         Update
                       </button>
@@ -262,7 +262,7 @@ const DiveCertification = () => {
                           setSelectedCertId(cert.id);
                           setShowDeleteModal(true);
                         }}
-                        className="block w-full text-left px-4 py-2 rounded-lg text-medium text-black hover:bg-[#2C7DA0] hover:bg-opacity-50"
+                        className="block w-full text-left px-4 py-2 rounded-lg text-medium text-[#001526] hover:bg-[#2C7DA0] hover:bg-opacity-50"
                       >
                         Delete
                       </button>
@@ -284,26 +284,26 @@ const DiveCertification = () => {
 
       {/* Delete Confirmation Modal */}
       {showDeleteModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-[#001526] bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-[#D9E7EC] p-6 rounded-3xl shadow-lg w-[680px] h-[600px] text-center">
 
             <div className="flex justify-center mb-4">
               <img src="/trash-delete.svg" alt="Delete" className="w-50 h-50 mt-40" />
             </div>
 
-            <h2 className="text-5xl font-bold text-black mt-10">Delete Certificate?</h2>
+            <h2 className="text-5xl font-bold text-[#001526] mt-10">Delete Certificate?</h2>
             <p className="text-xl font-semibold text-gray-600 mt-4">This action cannot be undone.</p>
 
             <div className="mt-20 flex justify-center space-x-4">
               <button
                 onClick={() => setShowDeleteModal(false)}
-                className="w-48 h-14 border border-black border-2 rounded-full font-semibold text-black hover:bg-black hover:text-white"
+                className="w-48 h-14 border border-[#001526] border-2 rounded-full font-semibold text-[#001526] hover:bg-[#001526] hover:text-white"
               >
                 Cancel
               </button>
               <button
                 onClick={deleteCertificate}
-                className="w-48 h-14 border border-black border-2 rounded-full font-semibold text-black hover:bg-black hover:text-white"
+                className="w-48 h-14 border border-[#001526] border-2 rounded-full font-semibold text-[#001526] hover:bg-[#001526] hover:text-white"
               >
                 Yes
               </button>
