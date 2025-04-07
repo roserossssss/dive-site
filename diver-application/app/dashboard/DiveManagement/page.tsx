63464e5ef2be2eeadfcf8f2691b11df0604c9884
roserossssss <156775235+roserossssss@users.noreply.github.com>
2025-04-07 13:37:10 +0800

    Updated Header Title
    - updated the header title for the page

--- diver-application/app/dashboard/DiveManagement/page.tsx
+++ diver-application/app/dashboard/DiveManagement/page.tsx
@@ -85,8 +85,8 @@ const confirmDelete = () => {
   return (
     <div>
     {/* Header Section */}
-    <div className="fixed top-0 left-0 w-full bg-white z-10 p-3 rounded-t-2xl md:pl-80">
-    <h2 className="text-2xl p-2 sm:text-2xl md:text-3xl text-black font-bold">My Dive</h2>
+    <div className="fixed top-0 left-0 w-full bg-white z-10 p-4 rounded-t-2xl md:pl-80">
+    <h2 className="text-xl md:text-xl lg:text-2xl font-bold text-[#001526] text-center md:text-left mt-2">My Dive</h2>
     </div>
     {/* Controls */}
     <div className="flex items-center justify-between gap-1 md:gap-4 mt-8 flex-nowrap">
