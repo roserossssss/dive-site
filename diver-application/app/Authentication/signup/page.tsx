aabdef0e23f745c03268318f08ff06e5597f3e0a
roserossssss <156775235+roserossssss@users.noreply.github.com>
2025-04-15 13:54:34 +0800

    Updated Placeholders
    - added a floating placeholders for labels

--- diver-application/app/Authentication/signup/page.tsx
+++ diver-application/app/Authentication/signup/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Link from 'next/link';
-import { CiCalendar } from "react-icons/ci";
-import { useRef, useState } from "react";
+import Link from "next/link";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
 import { Eye, EyeOff } from "lucide-react";
+import { CiCalendar } from "react-icons/ci";
 
 export default function Signup() {
   const dateInputRef = useRef(null);
@@ -13,16 +13,16 @@ export default function Signup() {
 
   return (
     <div className="relative min-h-screen bg-gray-200 flex items-center justify-center">
-      <Image 
-        src="/images/page_bg_v2.jpg"  
+      <Image
+        src="/images/page_bg_v2.jpg"
         alt="Landing Page"
         objectFit="cover"
-        priority 
+        priority
         className="absolute inset-0 w-full h-full"
         width={1500}
         height={0}
       />
-      
+
       <div className="relative z-10 flex w-full lg:w-2/4 justify-center items-center ml-auto lg:p-8 p-4">
         <div className="w-full max-w-xl lg:p-8 p-4">
           <form className="bg-[#D9E7EC] p-8 rounded-3xl shadow-lg w-full py-12 lg:py-16 px-12">
@@ -31,109 +31,159 @@ export default function Signup() {
                 Create Account
               </h1>
 
-              {/* First & Last Name */}
-              <input    
-                type="text" 
-                placeholder="First Name" 
-                className="w-full pl-4 p-2 mb-3 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-[#005f80] bg-[#D9E7EC] border-black border-t-1 text-[15px] text-[#001526]"
-                required 
-              />
-              <input    
-                type="text" 
-                placeholder="Last Name" 
-                className="w-full pl-4 p-2 mb-3 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-[#005f80] bg-[#D9E7EC] border-black border-t-1 text-[15px] text-[#001526]"
-                required 
-              />
+              {/* First Name */}
+              <div className="relative w-full mb-3">
+                <input
+                  type="text"
+                  id="firstName"
+                  placeholder=" "
+                  className="peer h-12 w-full border border-[#001526] rounded-3xl px-3 pt-4 pb-1 text-sm placeholder-transparent focus:outline-none focus:ring-2 focus:ring-[#005f80] focus:border-0 bg-[#D9E7EC] text-[#001526]"
+                  required
+                />
+                <label
+                  htmlFor="firstName"
+                  className="absolute left-3 top-1/5 -translate-y-1/2 text-xs transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs peer-focus:text-[#005f80] bg-[#D9E7EC] px-1"
+                >
+                  First Name
+                </label>
+              </div>
+
+              {/* Last Name */}
+              <div className="relative w-full mb-3">
+                <input
+                  type="text"
+                  id="lastName"
+                  placeholder=" "
+                  className="peer h-12 w-full border border-[#001526] rounded-3xl px-3 pt-4 pb-1 text-sm placeholder-transparent focus:outline-none focus:ring-2 focus:ring-[#005f80]  focus:border-0 bg-[#D9E7EC] text-[#001526]"
+                  required
+                />
+                <label
+                  htmlFor="lastName"
+                  className="absolute left-3 top-1/5 -translate-y-1/2 text-xs transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs peer-focus:text-[#005f80] bg-[#D9E7EC] px-1"
+                >
+                  Last Name
+                </label>
+              </div>
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full mb-3">
                 {/* Birthday Field */}
                 <div className="relative mt-1 mb-1">
-                  <label className="block text-[#001526] font-semibold ml-1 mb-1">Birthday</label>
-                  <input 
-                    type="date" 
-                    ref={dateInputRef} 
+                  <label className="block text-[#001526] font-semibold ml-1 mb-1">
+                    Birthday
+                  </label>
+                  <input
+                    type="date"
+                    ref={dateInputRef}
                     placeholder="Birthday"
-                    className="w-full pl-4 p-2 border rounded-3xl text-[#001526] text-[15px] focus:outline-none focus:ring-2 focus:ring-[#005f80] bg-[#D9E7EC] border-black border-t-1"
-                    required 
+                    className="w-full pl-4 p-2 border rounded-3xl text-[#001526] text-[15px] focus:outline-none focus:ring-2 focus:ring-[#005f80] focus:border-0 bg-[#D9E7EC] border-black border-t-1"
+                    required
                     style={{ appearance: "none" }}
                   />
-                  <CiCalendar 
-                    className="absolute right-4 top-9 text-[#001526] text-2xl cursor-pointer" 
-                    onClick={() => (dateInputRef.current as unknown as HTMLInputElement)?.showPicker()}
+                  <CiCalendar
+                    className="absolute right-4 top-9 text-[#001526] text-2xl cursor-pointer"
+                    onClick={() =>
+                      (dateInputRef.current as unknown as HTMLInputElement)?.showPicker()
+                    }
                   />
                 </div>
 
                 {/* Gender Selection */}
                 <div className="mt-1 mb-1">
-                  <label className="block text-[#001526] font-semibold ml-1 mb-1">Sex</label>
+                  <label className="block text-[#001526] font-semibold ml-1 mb-1">
+                    Sex
+                  </label>
                   <div className="flex items-center justify-center space-x-4 border border-black rounded-3xl p-2 bg-[#D9E7EC]">
                     <label className="flex items-center">
                       <input
                         type="radio"
                         name="gender"
                         value="male"
-                        className="mr-2 w-3 h-3 appearance-none rounded-full border-2 border-[#001526] checked:bg-[#001526] checked:border-[#001526] transition duration-200"
+                        className="mr-2 w-3 h-3 appearance-none rounded-full border-2 border-[#001526] checked:bg-[#005f80] checked:border-[#135572] transition duration-200"
                         required
                       />
-                      <span className="text-[#001526] text-[15px] font-medium">Male</span>
+                      <span className="text-[#001526] text-[15px] font-medium">
+                        Male
+                      </span>
                     </label>
-
                     <label className="flex items-center">
                       <input
                         type="radio"
                         name="gender"
                         value="female"
-                        className="mr-2 w-3 h-3 appearance-none rounded-full border-2 border-[#001526] checked:bg-[#001526] checked:border-[#001526] transition duration-200"
+                        className="mr-2 w-3 h-3 appearance-none rounded-full border-2 border-[#001526] checked:bg-[#005f80] checked:border-[#135572] transition duration-200"
                         required
                       />
-                      <span className="text-[#001526] text-[15px] font-medium">Female</span>
+                      <span className="text-[#001526] text-[15px] font-medium">
+                        Female
+                      </span>
                     </label>
                   </div>
                 </div>
               </div>
 
-              {/* Email */}
-              <input    
-                type="text" 
-                placeholder="Email Address" 
-                className="w-full pl-4 p-2 mb-3 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-[#005f80] bg-[#D9E7EC] border-black border-t-1 text-[15px] text-[#001526]"
-                required 
-              /> 
-              
-              <div className="w-full max-w-xl mx-auto px-0">
-                {/* Password */}
-                <div className="relative">
-                  <input 
-                    type={showPassword ? "text" : "password"} 
-                    placeholder="Password" 
-                    className="w-full pl-4 pr-10 p-2 mb-3 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-[#005f80] bg-[#D9E7EC] border-black text-[15px] text-[#001526]"
-                    required 
-                  />
-                  <button
-                    type="button"
-                    onClick={() => setShowPassword(!showPassword)}
-                    className="absolute right-4 top-1/3 -translate-y-1/3 text-[#001526]"
-                  >
-                    {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                  </button>
-                </div>
+              {/* Email Address */}
+              <div className="relative w-full mb-3">
+                <input
+                  type="email"
+                  id="email"
+                  placeholder=" "
+                  className="peer h-12 w-full border border-[#001526] rounded-3xl px-3 pt-4 pb-1 text-sm placeholder-transparent focus:outline-none focus:ring-2 focus:ring-[#005f80] focus:border-0 bg-[#D9E7EC] text-[#001526]"
+                  required
+                />
+                <label
+                  htmlFor="email"
+                  className="absolute left-3 top-1/5 -translate-y-1/2 text-xs transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs peer-focus:text-[#005f80] bg-[#D9E7EC] px-1"
+                >
+                  Email Address
+                </label>
+              </div>
 
-                {/* Re-enter Password */}
-                <div className="relative">
-                  <input 
-                    type={showRePassword ? "text" : "password"} 
-                    placeholder="Re-enter Password" 
-                    className="w-full pl-4 pr-10 p-2 mb-3 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-[#005f80] bg-[#D9E7EC] border-black text-[15px] text-[#001526]"
-                    required 
-                  />
-                  <button
-                    type="button"
-                    onClick={() => setShowRePassword(!showRePassword)}
-                    className="absolute right-4 top-1/3 -translate-y-1/3 text-[#001526]"
-                  >
-                    {showRePassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                  </button>
-                </div>
+              {/* Password */}
+              <div className="relative w-full mb-3">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  placeholder=" "
+                  className="peer h-12 w-full border border-[#001526] rounded-3xl px-3 pt-4 pb-1 text-sm placeholder-transparent focus:outline-none focus:ring-2 focus:ring-[#005f80] focus:border-0 bg-[#D9E7EC] text-[#001526]"
+                  required
+                />
+                <label
+                  htmlFor="password"
+                  className="absolute left-3 top-1/5 -translate-y-1/2 text-xs transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs peer-focus:text-[#005f80] bg-[#D9E7EC] px-1"
+                >
+                  Password
+                </label>
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-4 top-1/2 -translate-y-1/2 text-[#001526]"
+                >
+                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                </button>
+              </div>
+
+              {/* Re-enter Password */}
+              <div className="relative w-full mb-3">
+                <input
+                  type={showRePassword ? "text" : "password"}
+                  id="repassword"
+                  placeholder=" "
+                  className="peer h-12 w-full border border-[#001526] rounded-3xl px-3 pt-4 pb-1 text-sm placeholder-transparent focus:outline-none focus:ring-2 focus:ring-[#005f80] focus:border-0 bg-[#D9E7EC] text-[#001526]"
+                  required
+                />
+                <label
+                  htmlFor="repassword"
+                  className="absolute left-3 top-1/5 -translate-y-1/2 text-xs transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs peer-focus:text-[#005f80] bg-[#D9E7EC] px-1"
+                >
+                  Re-enter Password
+                </label>
+                <button
+                  type="button"
+                  onClick={() => setShowRePassword(!showRePassword)}
+                  className="absolute right-4 top-1/2 -translate-y-1/2 text-[#001526]"
+                >
+                  {showRePassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                </button>
               </div>
 
               {/* Signup Button */}
@@ -143,8 +193,13 @@ export default function Signup() {
 
               {/* Login Redirect */}
               <div className="flex justify-center mt-3 text-[15px]">
-                <span className="text-[#001526] font-bold">Already have an account?</span>
-                <Link href="/Authentication/login" className="text-[#005f80] hover:underline font-bold ml-2">
+                <span className="text-[#001526] font-bold">
+                  Already have an account?
+                </span>
+                <Link
+                  href="/Authentication/login"
+                  className="text-[#005f80] hover:underline font-bold ml-2"
+                >
                   Log-in
                 </Link>
               </div>
