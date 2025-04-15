8c93f47e0e7b318cfc7c393f06efb21e0ad2d178
roserossssss <156775235+roserossssss@users.noreply.github.com>
2025-04-15 11:06:12 +0800

    Upated Log In Page
    - added an eye icon to allow users to toggle their password visibility

--- diver-application/app/Authentication/login/page.tsx
+++ diver-application/app/Authentication/login/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Link from 'next/link';
 import Image from "next/image";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
 export default function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
         <div className="relative min-h-screen bg-gray-200 flex items-center justify-center">
             <Image 
@@ -17,36 +23,45 @@ export default function Login() {
                 <div className="w-full max-w-xl lg:p-8 p-4">
                     <form className="bg-[#D9E7EC] p-8 rounded-3xl shadow-lg w-full lg:pt-16 pt-5 lg:pb-24 pb-12 lg:pl-14 lg:pr-14">
                         <div className="flex flex-col items-center lg:px-6 px-4">
-                            <Image className="w-40 h-40 mb-4" src="../images/dive_light_logo_name.svg" alt="logo"  width={40} height={40}/>
-                            <h1 className="text-2xl font-bold text-center mb-6 text-[#001526]">
-                                Log in
-                            </h1>
+                            <Image className="w-40 h-40 mb-4" src="/images/dive_light_logo_name.svg" alt="logo" width={40} height={40} />
+                            <h1 className="text-2xl font-bold text-center mb-6 text-[#001526]">Log in</h1>
 
                             {/* Email Input */}
                             <input
                                 type="text" 
                                 placeholder="Email Address" 
-                                className="w-full pl-4 p-2 mb-2 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9E7EC] border-black border-t-1 text-[15px] text-[#001526]"
+                                className="w-full pl-4 p-2 mb-2 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-[#005f80] bg-[#D9E7EC] border-black border-t-1 text-[15px] text-[#001526]"
                                 required 
                             />
 
-                            {/* Password Input */}
-                            <input 
-                                type="password" 
-                                placeholder="Password" 
-                                className="w-full pl-4 p-2 mb-2 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9E7EC] border-black border-t-1 text-[15px] text-[#001526]"
-                                required 
-                            />
+                            {/* Password */}
+                            <div className="w-full max-w-xl mx-auto px-0">
+                                <div className="relative">
+                                    <input 
+                                        type={showPassword ? "text" : "password"} 
+                                        placeholder="Password" 
+                                        className="w-full pl-4 pr-10 p-2 mb-3 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-[#005f80] bg-[#D9E7EC] border-black text-[15px] text-[#001526]"
+                                        required 
+                                    />
+                                    <button
+                                        type="button"
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        className="absolute right-4 top-1/3 -translate-y-1/3 text-[#001526]"
+                                    >
+                                        {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                                    </button>
+                                </div>
+                            </div>
 
                             {/* Forgot Password Link */}
                             <div className="flex justify-end w-full mb-2 text-[15px]">
-                                <Link href="/forgot-password" className="text-[#001526] hover:underline font-bold">
+                                <Link href="/forgot-password" className="text-[#001526] hover:text-[#CF0C0F] font-bold">
                                     Forgot Password?
                                 </Link>
                             </div>
 
                             {/* Login Button */}
-                            <button className="w-full bg-[#001526] text-white py-2 rounded-3xl hover:bg-blue-700 transition mt-6 text-[15px] font-semibold">
+                            <button className="w-full bg-[#001526] text-white py-2 rounded-3xl hover:bg-[#005f80]  transition mt-6 text-[15px] font-semibold">
                                 Login
                             </button>
 
@@ -57,11 +72,10 @@ export default function Login() {
                                     Sign-up
                                 </Link>
                             </div>
-
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     );
-};
+}
