389cb5379db90e25757f94d675feae5b6a5856b3
TCJay_Append <123647533+TCAppend@users.noreply.github.com>
2025-05-05 09:31:41 +0800

    firebase update
    - fixed images not working

--- diver-application/next.config.ts
+++ diver-application/next.config.ts
@@ -3,10 +3,13 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   disable: false,
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
-    domains: ["media.istockphoto.com"],
+    
+    unoptimized: true,
+    //domains: ['media.istockphoto.com'],
   },
 };
 
