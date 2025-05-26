14114b5d344dd4de98ab2f88c2c657addb68524b
Princess Ong <107199349+jinjja-tet@users.noreply.github.com>
2025-05-26 09:22:06 +0800

    Adjustment

--- diver-application/app/Admin-Dashboard/CertificateManagement/page.tsx
+++ diver-application/app/Admin-Dashboard/CertificateManagement/page.tsx
@@ -162,7 +162,7 @@ export default function CertificateManagement() {
         Certification Management
       </h2>
 
-      <div className="-mt-7 p-5 min-h-[60vh] mr-0 md:mr-2">
+      <div className="-mt-7 p-5 min-h-[60vh] mr-0 md:mr-[0.65rem]">
         {/* Controls */}
         <div className="flex flex-wrap items-center justify-between gap-2 mt-12">
           {/* Navigation Arrows and Delete Button */}
