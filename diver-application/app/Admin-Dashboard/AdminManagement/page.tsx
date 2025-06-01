d54272f439378eeed52d36494016962b22e7617f
roserossssss <156775235+roserossssss@users.noreply.github.com>
2025-06-02 07:56:25 +0800

    Improved UI
    - few tweaks

--- diver-application/app/Admin-Dashboard/AdminManagement/page.tsx
+++ diver-application/app/Admin-Dashboard/AdminManagement/page.tsx
@@ -224,7 +224,7 @@ export default function UserManagement() {
                 <input
                   type="text"
                   placeholder="Search"
-                  className="pl-10 pr-4 py-2 rounded-full bg-white border border-[#001526] text-[#001526] font-medium placeholder-[#001526] focus:ring-2 focus:ring-[#001526] focus:border-blue-500 transition-all duration-300 ease-in-out text-xs sm:text-base w-16 md:w-20 lg:w-70 md:hover:w-38 md:focus-within:w-38 lg:focus-within:w-80"
+                  className="pl-10 pr-4 py-2 rounded-full bg-white border border-[#001526] text-[#001526] font-medium placeholder-[#001526] focus:ring-2 focus:ring-[#001526] focus:border-blue-500 transition-all duration-300 ease-in-out text-xs sm:text-base w-16 md:w-20 lg:w-72 md:hover:w-38 md:focus-within:w-38 lg:focus-within:w-80"
                   aria-label="Search"
                 />
               </div>
